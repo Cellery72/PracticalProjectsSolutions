b9a3ec5dd900ae728c10c04d6242674fcba8d465
ElleryJ <justin.ellery@opp.ca>
2017-09-01 12:47:36 -0400

    Add project files.

---
+++ PracticalProjectsSolutions/Networking.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticalProjectsSolutions
+{
+    public static class Networking
+    {
+    }
+}
---
+++ PracticalProjectsSolutions/Numbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PracticalProjectsSolutions
+{
+    public static class Numbers
+    {
+
+        public static int[] GenerateFibonacciSequenceNthNumber(int nth)
+        {
+            int[] fibSequence = {0,1};
+
+
+            for(int i=0;i<nth;++i)
+            {
+                var x = fibSequence[fibSequence.Length - 1] + fibSequence[fibSequence.Length - 2];
+                Console.WriteLine(x);
+            }
+            
+            return fibSequence;
+        }
+
+
+
+    }
+}
